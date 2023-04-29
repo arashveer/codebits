eaa144a79027f667f5754978da9852f3383fd99e
agaraya <agaraya@students.ubc.ca>
2023-04-28 23:09:11 -0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Codebits</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        First Commit!
+      </p>
+    </>
+  )
+}
+
+export default App
