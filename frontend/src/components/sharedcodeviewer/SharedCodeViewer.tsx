d6a3054be85d879523fffaa594dbe1b3c8f2da9a
agaraya <agaraya@students.ubc.ca>
2023-06-03 22:41:50 -0700

    share page start

--- frontend/src/components/sharedcodeviewer/SharedCodeViewer.tsx
+++ frontend/src/components/sharedcodeviewer/SharedCodeViewer.tsx
@@ -5,6 +5,9 @@ import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import { APIResponse } from "../../types";
 import { timeSince } from "../../utils/timeSince";
+import "../../styles/app.css";
+import "../../styles/editor.css";
+import "../../styles/viewer.css";
 
 export function SharedCodeViewer() {
   const [apiResponse, setApiResponse] = React.useState<APIResponse>();
@@ -35,20 +38,29 @@ export function SharedCodeViewer() {
 
   return (
     <div>
-      Last modified:{" "}
-      {apiResponse &&
-        timeSince(Date.parse(apiResponse.updatedAt.toString()).toString())}
+      <div className="file-name-header">{apiResponse?.title}</div>
+      <div className="flex info-container">
+        <div className="last-modified">
+          Created:{" "}
+          {apiResponse &&
+            new Date(apiResponse.createdAt.toString()).toLocaleDateString()}
+          {" | Last modified: "}
+          {apiResponse &&
+            timeSince(Date.parse(apiResponse.updatedAt.toString()).toString())}
+        </div>
+        <div>
+          <Link to={"/"} state={apiResponse}>
+            <button className="save-button">Make a copy</button>
+          </Link>
+        </div>
+      </div>
       <CodeMirror
         value={apiResponse?.code}
-        height="500px"
-        width="800px"
+        height="600px"
         theme="dark"
         extensions={[javascript({ jsx: true })]}
         editable={false}
       />
-      <Link to={"/"} state={apiResponse}>
-        Make a copy
-      </Link>
     </div>
   );
 }
